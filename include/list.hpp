af41e0beb845052655a7d6f0a463227220572f64
LiYang <272891021@qq.com>
2023-04-19 01:26:12 +0000

    list done enter the commit message for your changes. Lines starting

--- include/list.hpp
+++ include/list.hpp
@@ -9,32 +9,40 @@ private:
         node *next;
     };
 
+    node *begin_p = nullptr;
+    node *end_p = nullptr;
+    int length = 0;
+
+public:
     class Iter {
     private:
         node *p;
 
     public:
         Iter(node *n) : p(n) {}
-        Iter &operator++() {
+        Iter &operator++(int i) {
             p = p->next;
             return *this;
         }
-        bool operator==(Iter &other) { return p == other.p; }
-        bool operator!=(Iter &other) { return p != other.p; }
+        T &operator*() const { return p->data; }
+        bool operator==(const Iter &other) const { return p == other.p; }
+        bool operator!=(const Iter &other) const { return p != other.p; }
     };
-    int length = 0;
-    node *begin = nullptr;
-    node *end = nullptr;
 
-public:
     int len() { return length; };
+
     void append(T data) {
+        using namespace std;
         auto n_node = new node(data);
         if (length == 0) {
-            end = begin = n_node;
+            end_p = begin_p = n_node;
         } else {
-            end->next = n_node;
-            end = n_node;
+            end_p->next = n_node;
+            end_p = n_node;
         }
+        length += 1;
     }
+
+    Iter begin() { return Iter(begin_p); }
+    Iter end() { return Iter(nullptr); }
 };
--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include "include/List.hpp"
 #include <iostream>
 
+#include "include/list.hpp"
+
 int main() {
-  List<int> list;
-  std::cout << list.len() << std::endl;
-  return 0;
+    List<int> list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+    list.append(6);
+    std::cout << "length: " << list.len() << std::endl;
+    for (List<int>::Iter it = list.begin(); it != list.end();it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+    return 0;
 }
